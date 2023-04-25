8faaad2e8a4ae285a51293eba37ec4e892ed9fed
Khanya Maqula <khanyamaqula3@gmail.com>
2023-04-25 11:29:42 +0200

    completed the 2 challenges

---
+++ script2.js
@@ -0,0 +1,44 @@
+// script.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    const multiplied = this.multiply(added, this.internal.c);
+    return multiplied;
+    console.log(multiplied)
+  }
+  
+  // Not allowed to change below this
+  
+  const example1 = {
+    internal: {
+      a: 2,
+      b: 4,
+      c: 8,
+    },
+    add,
+    multiply,
+    calculate: internal,
+  };
+  
+  const example2 = {
+    internal: {
+      a: 2,
+      b: 2,
+      c: 3,
+    },
+    add,
+    multiply,
+    calculate: internal,
+  };
+  
+  console.log(example1.calculate()); // Output: 8
+  console.log(example2.calculate()); // Output: 4
+  
